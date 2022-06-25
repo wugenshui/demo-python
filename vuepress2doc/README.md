f11badf0c5c97c7d8a7561dcae3ea09d9e9cdfbb
chenbo <963838470@qq.com>
2022-06-25 13:59:13 +0800

    links 和 img 地址补全

--- vuepress2doc/README.md
+++ vuepress2doc/README.md
@@ -1,12 +1,22 @@
+# 运行说明
+
+## 安装依赖组件
+https://wkhtmltopdf.org/downloads.html
+
+## 批量安装依赖
 cd vuepress2doc
-# 批量安装依赖
 pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
 python index.py
 
-# 将依赖包写入 requirements.txt
+## 其它
+
+### 将依赖包写入 requirements.txt
 pip freeze > requirements.txt
 
-# 单个依赖安装 
+### 单个依赖安装 
 pip install pdfkit -i https://mirrors.aliyun.com/pypi/simple/
 pip install requests -i https://mirrors.aliyun.com/pypi/simple/
 pip install beautifulsoup4 -i https://mirrors.aliyun.com/pypi/simple/
+
+### 参考文档
+https://segmentfault.com/a/1190000019132150
--- vuepress2doc/index.py
+++ vuepress2doc/index.py
@@ -2,7 +2,7 @@ import pdfkit
 import requests
 from bs4 import BeautifulSoup	
 
-def get_content_and_next_url(content): # content 为网页内容
+def get_content_and_next_url(urlPrefix, content): # content 为网页内容
     soup = BeautifulSoup(content)	 # 得到文档的对象
 
     with open('1.html', 'wb') as f:
@@ -37,12 +37,26 @@ def get_content_and_next_url(content): # content 为网页内容
     if len(page_nav):
         page_nav[0].decompose()
 
+    links = soup.select('link')
+    for link in links:
+      if not link['href'].startswith("http"):
+        link['href'] = urlPrefix + link['href'] # 将地址补全
+
+    imgs = soup.select('img')
+    for img in imgs:
+      if img['src'].startswith("/"):
+        img['src'] = urlPrefix + img['src'] # 将地址补全
+
     with open('2.html', 'wb') as f:
         f.write(bytes(soup.prettify(),'UTF-8'))
 
-req = requests.get('http://192.168.0.51:8892/guide/')
+url = 'http://192.168.0.51:8892/guide/'
+req = requests.get(url)
 
-content = get_content_and_next_url(req.content)
+urls = url.split('/')
+urlPrefix = urls[0] + '//' + urls[2]
+
+content = get_content_and_next_url(urlPrefix, req.content)
 
 pdfkit.from_file(["2.html"], 'out.pdf', configuration=pdfkit.configuration(wkhtmltopdf="D:/lib/wkhtmltopdf/bin/wkhtmltopdf.exe"))
 
