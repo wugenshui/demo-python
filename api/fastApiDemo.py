09605c2734f79ac36d411215620a6d8fd030a818
chenbo <chenbo@huaweisoft.com>
2024-01-09 16:01:47 +0800

    fastapi demo

---
+++ api/fastApiDemo.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def index():
+    """
+    注册一个根路径
+    :return:
+    """
+    return {"message": "Hello World"}
+
+@app.get("/info")
+async def info():
+    """
+    项目信息
+    :return:
+    """
+    return {
+        "app_name": "FastAPI框架学习",
+        "app_version": "v0.0.1"
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
