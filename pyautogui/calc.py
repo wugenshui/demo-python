5d056df2c950ff3e55475f7efcaac78731198e84
chenbo <chenbo@huaweisoft.com>
2024-03-30 08:58:19 +0800

    简单的计算程序

--- pyautogui/calc.py
+++ pyautogui/calc.py
@@ -2,18 +2,16 @@ import pyautogui
 # 使用pyautogui打开计算器输入10+5=，然后按回车键计算结果
 # 打开计算器
 pyautogui.hotkey('winleft')
-pyautogui.typewrite('calc\n', interval=0.25)
+pyautogui.typewrite('calc')
+# 点击回车
+pyautogui.press('enter')
 # 等待计算器打开
 pyautogui.sleep(1)
 # 输入10
 pyautogui.typewrite('10')
 # 点击+
-pyautogui.click(x=160, y=380)
+pyautogui.click(x=570, y=670)
 # 输入5
 pyautogui.typewrite('5')
 # 点击=
-pyautogui.click(x=400, y=380)
-# 点击回车
-pyautogui.press('enter')
-# 等待结果
-pyautogui.sleep(1)
+pyautogui.click(x=570, y=730)
